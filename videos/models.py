358c7962360adbb893ba909ac914a2e98c060f5c
worlddeleteRin <worlddelete0@gmail.com>
2022-06-18 02:11:46 +0300

    :bug: fix typo

--- videos/models.py
+++ videos/models.py
@@ -5,7 +5,7 @@ from yt_core.selenium.main import ScreenDimensionEnum
 
 class SeleniumWatchVideoParams(BaseModel):
     instances_count: int=4
-    screen_dimenstion: ScreenDimensionEnum = ScreenDimensionEnum.svga
+    screen_dimension: ScreenDimensionEnum = ScreenDimensionEnum.svga
     headless: bool=True
     video_link: str
     play_btn_class: str="ytp-play-button"
